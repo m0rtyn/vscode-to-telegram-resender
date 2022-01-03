e5641e15f750773771ad2ba02b4be3f11fff40df
Martyn <zogacc@gmail.com>
2022-01-03 22:33:13 +0200

    init server

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const express = require("express");
 const axios = require("axios").default;
-const cron = require("node-cron");
 const app = express();
 const endpoint = "https://be-b010.herokuapp.com/api/info";
 
-function callApi() {
-  axios({
-    method: "get",
-    url: endpoint,
-  }).then(function (response) {
-    console.log(response);
-  });
-}
-
-cron.schedule("*/1 * * * *", () => {
-    callApi();
-});
-
 app.listen(3000, () => console.log("App is running on port 3000"));
+app.post('post', (request) => {
+  const payload = request.body
+  const postDelay = 60
+
+  setTimeout(() => {
+    const data = {
+      ...payload,
+    }
+    axios.post("https://api.telegram.org/bot1427261129:AAEHCboM0MC81MI5qYv4t2lg6phVcBYXTL4/sendMessage", data)
+  }, postDelay)
+})
