75e9104a24afcc1eb7d2ed875ed7b019e5705c02
Martyn <zogacc@gmail.com>
2022-01-03 22:44:02 +0200

    refactor code

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const axios = require("axios").default;
 const app = express();
 const endpoint = "https://be-b010.herokuapp.com/api/info";
 
-app.listen(3000, () => console.log("App is running on port 3000"));
-app.post('post', (request) => {
+const PORT = 3000
+app.post('post', (req, res) => {
   const payload = request.body
   const postDelay = 60
 
+  res.send('Post scheduled')
+  
   setTimeout(() => {
     const data = {
       ...payload,
@@ -15,3 +17,5 @@ app.post('post', (request) => {
     axios.post("https://api.telegram.org/bot1427261129:AAEHCboM0MC81MI5qYv4t2lg6phVcBYXTL4/sendMessage", data)
   }, postDelay)
 })
+
+app.listen(PORT, () => console.log("App is running on port 3000"));
