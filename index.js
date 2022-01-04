9b704aec9e442e9a4590b59bfe296bc416491923
Martyn <zogacc@gmail.com>
2022-01-04 09:48:50 +0200

    make working implementation

--- index.js
+++ index.js
@@ -1,19 +1,38 @@
-const express = require("express");
-const axios = require("axios").default;
-const app = express();
-const endpoint = "https://be-b010.herokuapp.com/api/info";
+const express = require("express")
+const bodyParser = require("body-parser")
+const axios = require("axios").default
+
+const BOT_TOKEN = "1427261129:AAEHCboM0MC81MI5qYv4t2lg6phVcBYXTL4"
+const app = express()
+const jsonParser = bodyParser.json()
 
 const PORT = 3000
-app.post('post', (req, res) => {
-  const payload = request.body
-  const postDelay = 60
+app.get("", () => {
+  console.log("BANG")
+})
 
-  res.send('Post scheduled')
-  
+app.post("/posting", jsonParser, (req, res) => {
+  const payload = req.body
+  const postDelay = 3 * 1000 // seconds
+
+  res.on("error", (error) => {
+    console.log(error)
+  })
+
   setTimeout(() => {
-    const data = payload
-    axios.post("https://api.telegram.org/bot1427261129:AAEHCboM0MC81MI5qYv4t2lg6phVcBYXTL4/sendMessage", data)
+    axios
+      .post(
+        `https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`,
+        payload
+      )
+      .then((res) => {
+        console.log(res)
+        res.send("Post scheduled")
+      })
+      .catch((e) => {
+        console.error(e)
+      })
   }, postDelay)
 })
 
-app.listen(PORT, () => console.log("App is running on port 3000"));
+app.listen(PORT, () => console.log("App is running on port " + PORT))
